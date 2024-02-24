33a158af0e2e4a8a490d5ae61b927505cccdd312
amritangshu <deyamritangshu@gmail.com>
2024-02-24 23:43:58 +0530

    name change

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PageForm (@CodeWithKliton)",
-  description: "Check my youtube channel for more content",
+  title: "Form Builder @amrit",
+  description: "A simple and scalable form builder for your next project.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
